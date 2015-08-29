685291bb9f35d9c23313f8229ac3506cd765053f
reaganthomas <reagan_thomas@baylor.edu>
2015-08-29 00:12:44 -0500

    update gulp and package

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,18 @@
 
   var gulp = require('gulp');
   var mocha = require('gulp-mocha');
+  var del = require('del');
+
+  // gulp.task('clean', function(cb) {
+  //   del(['dist/**/*','lib/**/*'], cb);
+  // });
 
   gulp.task('test', function() {
     return gulp.src('test/index.js', { read: false })
       .pipe(mocha());
   });
+
+  // gulp.task('package', [], function() {
+  //
+  // });
 })();
